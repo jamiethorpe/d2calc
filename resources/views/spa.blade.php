32d7208c227e971f7b575c071ffbbf8f6ceeb56e
vagrant <jamie.thrp@gmail.com>
2019-03-28 19:32:02 -0400

    Add GA

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -13,6 +13,15 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137234754-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-137234754-1');
+        </script>
     </head>
     <body>
         <div id="app">
